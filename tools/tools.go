1ea98f9f0743c0e60e11980286f7c467f985e4cf
Anurag Kumar <abhisunny282@gmail.com>
2024-10-30 16:15:37 +0530

    wip: add build task and golnagci-lint and govulncheck

--- tools/tools.go
+++ tools/tools.go
@@ -2,6 +2,8 @@ package tools
 
 import (
 	_ "github.com/bufbuild/buf/cmd/buf"
+	_ "golang.org/x/vuln/cmd/govulncheck"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/google/yamlfmt/cmd/yamlfmt"
 	_ "github.com/pressly/goose/v3/cmd/goose"
 	_ "github.com/sqlc-dev/sqlc/cmd/sqlc"
